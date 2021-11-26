06a6f7a061c0ba17ea1f33d2a6032e4158e7bd5c
nilushi_voiceiq <nilushi@voiceiq.ai>
2021-11-27 00:10:24 +0530

    Project initiated with relevant best practices tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
+import Main from './containers/common/main/Main'
+
+import { appTheme } from './theme/Theme'
+import GlobalStyles from './theme/GlobalStyles'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={appTheme}>
+      <GlobalStyles />
+      <Router>
+        <Main />
+      </Router>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
